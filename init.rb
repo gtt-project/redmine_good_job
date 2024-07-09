17be466e2719791f29f7b9801e2542a3550a6227
Daniel Kastl <daniel@georepublic.de>
2024-07-09 16:45:59 +0900

    Sets 1.0.0 version

--- init.rb
+++ init.rb
@@ -7,7 +7,7 @@ Redmine::Plugin.register :redmine_good_job do
   author_url 'https://github.com/georepublic'
   url 'https://github.com/gtt-project/redmine_good_job'
   description 'Add support for Good Job background job processing to Redmine.'
-  version '0.0.1'
+  version '1.0.0'
 
   requires_redmine version_or_higher: '5.1.0'
 end
